b7492a2e92727c6906f9594cfc889c3bd94b6ae4
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-11-12 11:44:42 +0100

    Products
    Product informations shows based on search result

--- webbshopen/src/components/ProductContainer.tsx
+++ webbshopen/src/components/ProductContainer.tsx
@@ -1,8 +1,21 @@
+import Product from "./Product";
 import ProductContainerProps from "../types/ProductContainerProps";
+import ProductData from "../products.json"
 
 export default function ProductContainer(props: ProductContainerProps) {
+  const filteredProductsList = ProductData.filter((product) => (
+    product.name.toLowerCase().includes(props.query.toLowerCase())
+  ))
+
   return (
     <>
+      {filteredProductsList.length > 0 ? (
+        filteredProductsList.map((product) => (
+          <Product key={product.id} name={product.name} price={product.price} description={product.description}/>
+        ))
+      ) : (
+        <p>No Results</p>
+      )}
       <p>ProductContainer: {props.query}</p>
     </>
   );
